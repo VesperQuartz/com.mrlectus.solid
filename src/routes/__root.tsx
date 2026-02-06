b40de7c6710c7b3174681edc87b23ec2195aade9
VesperQuartz <lilbrown3000@gmail.com>
2026-02-06 11:29:17 +0100

    init (1)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,40 +1,39 @@
 import {
-  HeadContent,
-  Outlet,
-  Scripts,
-  createRootRouteWithContext,
-} from '@tanstack/solid-router'
-import { TanStackRouterDevtools } from '@tanstack/solid-router-devtools'
-import TanStackQueryProvider from '../integrations/tanstack-query/provider.tsx'
+	createRootRouteWithContext,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/solid-router";
+import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools";
+import { Suspense } from "solid-js";
 
-import { HydrationScript } from 'solid-js/web'
-import { Suspense } from 'solid-js'
-
-import styleCss from '../globals.css?url'
+import { HydrationScript } from "solid-js/web";
+import styleCss from "../globals.css?url";
+import TanStackQueryProvider from "../integrations/tanstack-query/provider.tsx";
 
 export const Route = createRootRouteWithContext()({
-  head: () => ({
-    links: [{ rel: 'stylesheet', href: styleCss }],
-  }),
-  shellComponent: RootComponent,
-})
+	head: () => ({
+		links: [{ rel: "stylesheet", href: styleCss }],
+	}),
+	shellComponent: RootComponent,
+});
 
 function RootComponent() {
-  return (
-    <html>
-      <head>
-        <HydrationScript />
-      </head>
-      <body>
-        <HeadContent />
-        <Suspense>
-          <TanStackQueryProvider>
-            <Outlet />
-            <TanStackRouterDevtools position="bottom-left" />
-          </TanStackQueryProvider>
-        </Suspense>
-        <Scripts />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<head>
+				<HydrationScript />
+			</head>
+			<body>
+				<HeadContent />
+				<Suspense>
+					<TanStackQueryProvider>
+						<Outlet />
+						<TanStackRouterDevtools position="bottom-left" />
+					</TanStackQueryProvider>
+				</Suspense>
+				<Scripts />
+			</body>
+		</html>
+	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-
-import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
-import solidPlugin from 'vite-plugin-solid'
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/solid-start/plugin/vite";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [
-    devtools(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    solidPlugin({ ssr: true }),
-  ],
-})
+	resolve: {
+		tsconfigPaths: true,
+	},
+	plugins: [
+		tailwindcss(),
+		devtools(),
+		tanstackStart(),
+		solidPlugin({ ssr: true }),
+		nitro({
+			preset: "bun",
+			compressPublicAssets: true,
+			prerender: {
+				crawlLinks: true,
+			},
+		}),
+	],
+});
